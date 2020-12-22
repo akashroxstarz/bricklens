75a9c9288b2259e89b103779b85bdeddb00cf5b4
Matt Welsh <mdw@octoml.ai>
2020-12-22 13:58:31 -0800

    Fixing up mess generation.

--- python/bricklens/render/generate_mess.py
+++ python/bricklens/render/generate_mess.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
+import argparse
 import os
 import random
+import subprocess
 import tempfile
+from typing import Any, List, Tuple
 
 from ldraw.figure import *
 from ldraw.library.colours import *
@@ -24,7 +27,6 @@ PARTS = [
 
 COLORS = list(ColoursByCode.values())
 
-NUM_PARTS = 1000
 
 POV_TRAILER = """
 light_source
@@ -39,33 +41,41 @@ light_source
   area_light x*70, y*70, 20, 20 circular orient adaptive 0 jitter
 }
 
-background { color White }
+background { color Gray }
 
 camera {
   location <-500.000000, 700.000000, -500.000000>
-  look_at <500.000000, -100.000000, 500.000000>
-  angle 50
+  look_at <500.000000, -200.000000, 500.000000>
+  angle 40
 }
 """
 
 
-def gen_parts():
-    parts = []
-    for _ in range(NUM_PARTS):
-        part = random.choice(PARTS)
-        color = random.choice(COLORS)
+def gen_parts(
+    parts: List[Any],
+    colors: List[Any],
+    num_parts: int = 1000,
+    x_range: Tuple[int, int] = (-500, 2000),
+    y_range: Tuple[int, int] = (0, 100),
+    z_range: Tuple[int, int] = (-500, 2000),
+):
+    """Generate a POVRay scene with `num_parts` random parts in a pile."""
+    retval = []
+    for _ in range(num_parts):
+        part = random.choice(parts)
+        color = random.choice(colors)
 
-        x = random.randint(0, 1000)
-        y = random.randint(0, 100)
-        z = random.randint(0, 1000)
+        x = random.randint(x_range[0], x_range[1])
+        y = random.randint(y_range[0], y_range[1])
+        z = random.randint(z_range[0], z_range[1])
 
         xrot = random.randint(0, 360) - 180
         yrot = random.randint(0, 360) - 180
         zrot = random.randint(0, 360) - 180
         rot = Identity().rotate(x, XAxis).rotate(yrot, YAxis).rotate(zrot, ZAxis)
 
-        parts.append(Piece(color, Vector(x, y, z), rot, part))
-    return parts
+        retval.append(Piece(color, Vector(x, y, z), rot, part))
+    return retval
 
 
 def gen_pov(ldraw_path, pov_path):
@@ -78,15 +88,50 @@ def gen_pov(ldraw_path, pov_path):
         pov_file.write(POV_TRAILER + "\n")
 
 
+def run_pov(pov_path, image_path, image_width, image_height):
+    cmd = [
+        "povray",
+        f"-i{pov_path}",
+        f"+W{image_width}",
+        f"+H{image_height}",
+        "+FN",
+        f"-o{image_path}",
+    ]
+    result = subprocess.run(cmd, capture_output=True)
+    result.check_returncode()
+
+
 def main():
-    parts = gen_parts()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--outfile", help="Output file for generated image.", required=True
+    )
+    parser.add_argument(
+        "--width",
+        help="Width in pixels of output image.",
+        default=1024,
+    )
+    parser.add_argument(
+        "--height",
+        help="Height in pixels of output image.",
+        default=768,
+    )
+    parser.add_argument(
+        "--num_parts",
+        help="Number of parts in the pile.",
+        default=10000,
+    )
+    args = parser.parse_args()
+
+    parts = gen_parts(PARTS, COLORS, args.num_parts)
     with tempfile.NamedTemporaryFile(mode="w", delete=False, suffix=".ldr") as ldr:
         for part in parts:
             ldr.write(str(part) + "\n")
     with tempfile.NamedTemporaryFile(mode="w", delete=False, suffix=".pov") as pov:
         pov.close()
         gen_pov(ldr.name, pov.name)
-    print(f"povray -i{pov.name} +W1024 +H768 +fp -o- > o.png && open o.png")
+        run_pov(pov.name, args.outfile, args.width, args.height)
+        print(f"Wrote output file {args.outfile}")
 
 
 if __name__ == "__main__":
