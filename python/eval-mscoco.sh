5bb9831c4dcbb2ede77d9489114220184435cd79
Matt Welsh <mdw@octoml.ai>
2021-07-11 10:58:09 -0700

    Removing unused files.

--- python/eval-mscoco.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-poetry run bricklens/train/detection/eval.py \
-  --num_images 100 \
-  --model_config_path bricklens/train/detection/config/yolov3-mscoco-pjreddie.yml \
-  --weights_path bricklens/train/detection/config/yolov3.weights \
-  --mscoco_path  ~/datasets/mscoco
--- python/eval.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-poetry run bricklens/train/detection/eval.py \
-  --weights_path checkpoints_simple_1000/checkpoint_99.weights \
-  --dataset_config_path bricklens/train/detection/config/dataset_detection_simple_1000.yml \
-  --slice train
