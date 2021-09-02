243f46a40dd7b507e086d4a85e7522830fdbd2a5
Matt Welsh <mdw@octoml.ai>
2021-09-01 22:15:32 -0700

    Parallelism and GCS copy

--- render/deploy/render.sh
+++ render/deploy/render.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
+# Script run by each K8s Job.
+
 set -eux
 
-echo "Starting render job..."
+echo "BRICKLENS_TIMESTAMP is: ${BRICKLENS_TIMESTAMP}"
+echo "BRICKLENS_JOB_ID is: ${BRICKLENS_JOB_ID}"
+
+echo "Starting render job."
 
 python3 -m bricklens.render.generate_detection_dataset \
   --ldraw_library_path /ldraw \
   --outdir /dataset \
   --overwrite \
   --num_images 2
+
+echo "Copying data to GCS..."
+
+gsutil cp -r \
+  /dataset \
+  gs://bricklens-datasets/renders/${BRICKLENS_TIMESTAMP}/${BRICKLENS_JOB_ID}/
 
 echo "Done."
